3e67f3a01392dfd5a9f8176d33108ca7a76f6319
kevin hu <hu.kevin.w@gmail.com>
2017-08-09 19:44:08 -0400

    Create component for job history

---
+++ src/components/jobs/JobHistory.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class JobHistory extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is the job history compon </p>
+      </div>
+    )
+  };
+};
+export default JobHistory
