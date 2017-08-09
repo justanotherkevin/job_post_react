dc966ce70c51e700d9180ba6775bb6b558336ab1
kevin hu <hu.kevin.w@gmail.com>
2017-08-09 18:51:26 -0400

    Add import jobinfo component

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
 import Search from './SearchBar'
+import AllJobs from './jobs/AllJobs'
+import JobInfo from './jobs/JobInfo'
+
+
+
 class App extends Component {
   render() {
+
+
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React</h2>
           <Search />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <AllJobs />
+          <JobInfo />
+        </div>
       </div>
     );
   }
