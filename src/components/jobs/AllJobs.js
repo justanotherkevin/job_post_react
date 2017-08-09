a477e50c5904dad956e363bd83619a7e16fbd01a
kevin hu <hu.kevin.w@gmail.com>
2017-08-09 18:51:50 -0400

    Create jobinfo component

---
+++ src/components/jobs/AllJobs.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AllJobs extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is the All job compon </p>
+      </div>
+    )
+  };
+};
+export default AllJobs
---
+++ src/components/jobs/JobInfo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class JobInfo extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is the job info </p>
+      </div>
+    )
+  };
+};
+export default JobInfo
