167abe7a81e94c7c19168b9b867ce5aff783859f
kevin hu <hu.kevin.w@gmail.com>
2017-08-09 18:03:35 -0400

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is the search bar </p>
+      </div>
+    )
+  };
+};
+export default Search
