18ef777f1b65884ea20676443c77b5f71248ac93
kevin hu <hu.kevin.w@gmail.com>
2017-08-09 20:27:16 -0400

    Import seed data and create state for alljobs

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
+import React, {Component} from 'react';
 import '../styles/App.css';
-import Search from './SearchBar'
-import AllJobs from './jobs/AllJobs'
-import JobInfo from './jobs/JobInfo'
-import JobHistory from './jobs/JobHistory'
+// import data for testing
+import jobsData    from '../data/allJobData';
+
+import Search     from './SearchBar';
+import AllJobs    from './jobs/AllJobs';
+import JobInfo    from './jobs/JobInfo';
+import JobHistory from './jobs/JobHistory';
+
+import axios      from 'axios';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allJobsData: []
+    };
+  };
 
+  componentDidMount() {
+    // axios.get('http://localhost:3000/job_posts').then(function(response) {
+    //   console.log(response);
+    // }).catch(function(error) {
+    //   console.log(error);
+    // });
+
+    const jobPosts = jobsData;
+    this.setState( {allJobsData: jobPosts} );
+  };
+
+  render() {
 
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React</h2>
-          <Search />
+          <Search/>
         </div>
         <div>
-          <JobHistory />
-          <AllJobs />
-          <JobInfo />
+          <JobHistory/>
+          <AllJobs/>
+          <JobInfo/>
         </div>
       </div>
     );
