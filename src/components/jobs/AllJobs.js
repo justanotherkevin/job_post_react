33b4a2fca88fb126ead832a719ad8baa0d512fca
kevin hu <hu.kevin.w@gmail.com>
2017-08-09 20:47:08 -0400

    Render each job from props

--- src/components/jobs/AllJobs.js
+++ src/components/jobs/AllJobs.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 
 class AllJobs extends Component {
   render() {
+    const { allJobs } = this.props
+    const eachJob = allJobs.map(( job, index ) =>
+      <div className="one_job" key={job.id} >
+        <h3>{job.title}</h3>
+        <p>{job.body}</p>
+      </div>
+    )
     return (
-      <div>
+      <div >
         <p>this is the All job compon </p>
+        {eachJob}
       </div>
     )
   };
