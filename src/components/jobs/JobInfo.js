4999d712b8f52dc9fa4b6065ecd8debcdb485785
kevin hu <hu.kevin.w@gmail.com>
2017-08-10 18:21:49 -0400

    Add wrapper class to render div

--- src/components/jobs/JobInfo.js
+++ src/components/jobs/JobInfo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class JobInfo extends Component {
   render() {
     return (
-      <div>
+      <div className="job_info_wrapper side_div">
         <p>this is the job info </p>
       </div>
     )
