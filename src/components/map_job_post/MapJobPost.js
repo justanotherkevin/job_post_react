274ba4ccbb8aa51ef652665bbf1e0241f8185d50
kevin hu <hu.kevin.w@gmail.com>
2017-08-11 15:20:00 -0400

    Add switch statement for rendering each possible component

--- src/components/map_job_post/MapJobPost.js
+++ src/components/map_job_post/MapJobPost.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 // import JobInfo    from './jobs/JobInfo';
-
-
+import Map from './Map';
+import JobInfo from './JobInfo';
+import NewPost from './NewPost';
 
 class MapJobPost extends Component {
   constructor(props) {
@@ -11,14 +12,25 @@ class MapJobPost extends Component {
     };
   };
 
-  render() {
-      // <JobInfo jobInfo={ jobInfoData }/>
+  notification(show)  {
+      switch(show) {
+          case 'map':
+              return <Map />;
+          case 'job_info':
+              return <JobInfo />;
+          case 'new_post':
+              return <NewPost />;
+          default:
+              return null;
+      }
+  }
 
+  render() {
 
     return (
       <div className='map_job_post_wrapper'>
         <p>this is the map job and post compon </p>
-
+        {this.notification(this.state.show)}
 
       </div>
     )
