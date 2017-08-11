4ccd642cf394d6d76b7c8a77cf413be81bfc831e
kevin hu <hu.kevin.w@gmail.com>
2017-08-11 10:49:04 -0400

    Move individual sample job info to SampleJob compon

--- src/components/jobs/AllJobs.js
+++ src/components/jobs/AllJobs.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
+import JobSample from './JobSample';
 
 class AllJobs extends Component {
-  render() {
-    const { allJobs } = this.props
 
-    const eachJob = allJobs.map(( job, index ) =>
-      <div className="one_job" key={job.id} >
-        <h3>{job.title}</h3>
-        <p>{job.company_name} - {job.location}</p>
-        <p>{job.summary.split(' ').slice(0, 15).join(' ')+"..."}</p>
-        <p>Desire skills: {job.skills} </p>
-        <p>Posted on: {job.date_created}</p>
-      </div>
-    )
+  render() {
+    // better for performance if it was not set in variable first
+    // const eachJob = this.props.allJobs.map(( job, index ) =>
+    //   <JobSample key={ job.id } job={ job } changeStateJobInfoData={ this.props.changeStateJobInfoData }/>
+    // )
 
     return (
       <div className="all_jobs_wrapper">
         <p>this is the All job compon </p>
-        {eachJob}
+        {this.props.allJobs.map(
+          ( job, index ) =>
+          <JobSample key={ job.id } job={ job } changeStateJobInfoData={ this.props.changeStateJobInfoData }/>
+        )}
       </div>
     )
   };
 };
+
 export default AllJobs
