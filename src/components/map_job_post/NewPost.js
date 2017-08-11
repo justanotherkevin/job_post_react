672ef420686dd20ee20c9bc23474027e6afa21a9
kevin hu <hu.kevin.w@gmail.com>
2017-08-11 15:21:19 -0400

    Create new component to house NewPost

---
+++ src/components/map_job_post/NewPost.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NewPost extends Component {
+  render() {
+    return (
+      <div>
+        <span>hello this is new_post </span>
+      </div>
+    )
+  }
+}
+export default NewPost;
