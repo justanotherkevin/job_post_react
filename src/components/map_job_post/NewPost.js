374acb503d79967c73de8816fb80eecf245afab6
kevin hu <hu.kevin.w@gmail.com>
2017-08-11 18:24:25 -0400

    Create form for new post

--- src/components/map_job_post/NewPost.js
+++ src/components/map_job_post/NewPost.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 
 class NewPost extends Component {
+
+  postAJob(data) {
+    console.log(data);
+    // axios.post('/job_posts', {
+    //   firstName: 'Fred',
+    //   lastName: 'Flintstone'
+    // })
+    // .then(function (response) {
+    //   console.log(response);
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
+  }
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
   render() {
     return (
-      <div>
-        <span>hello this is new_post </span>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     )
   }
 }
