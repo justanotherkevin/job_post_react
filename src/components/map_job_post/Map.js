ed8c89e41841bf7d26461bb4ae4935051709807a
kevin hu <hu.kevin.w@gmail.com>
2017-08-11 15:16:54 -0400

    Create map component

---
+++ src/components/map_job_post/Map.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+  render() {
+    return (
+      <div>
+        <span>hello this is map </span>
+      </div>
+    )
+  }
+}
+export default Map;
