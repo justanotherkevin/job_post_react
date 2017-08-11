4e6c14c841f4a867d3c4c17108b34de2caa45784
kevin hu <hu.kevin.w@gmail.com>
2017-08-10 20:42:49 -0400

    Render job detials

--- src/components/jobs/AllJobs.js
+++ src/components/jobs/AllJobs.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 class AllJobs extends Component {
   render() {
     const { allJobs } = this.props
+
     const eachJob = allJobs.map(( job, index ) =>
       <div className="one_job" key={job.id} >
         <h3>{job.title}</h3>
-        <p>{job.body}</p>
+        <p>{job.company_name} - {job.location}</p>
+        <p>{job.summary.split(' ').slice(0, 15).join(' ')+"..."}</p>
+        <p>Desire skills: {job.skills} </p>
+        <p>Posted on: {job.date_created}</p>
       </div>
     )
+
     return (
       <div className="all_jobs_wrapper">
         <p>this is the All job compon </p>
