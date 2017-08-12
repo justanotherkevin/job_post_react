80095a4e1358b9f9d70cbe30000ca824952aa844
kevin hu <hu.kevin.w@gmail.com>
2017-08-12 16:31:03 -0400

    Add general field for post form

--- src/components/map_job_post/NewPost.js
+++ src/components/map_job_post/NewPost.js
@@ -17,10 +17,13 @@ class NewPost extends Component {
   }
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      value: ''
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getSummaryField = this.getSummaryField.bind(this);
   }
 
   handleChange(event) {
@@ -29,17 +32,38 @@ class NewPost extends Component {
 
   handleSubmit(event) {
     alert('A name was submitted: ' + this.state.value);
+    
     event.preventDefault();
   }
-  render() {
+
+  getSummaryField() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <textarea
+        componentClass="textarea"
+        autoFocus={true}
+        value={this.state.value}
+        onChange={this.handleChange} />
+    )
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h3>This is posing a job!</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input value="Job Title" />
+          <input value="Company Name" />
+          <input value="Location" />
+          <input value="Skills" />
+
+          <label>
+            {this.getSummaryField()}
+          </label>
+          <input type="submit" value="Submit" />
+
+        </form>
+      </div>
     )
   }
 }
