2b94988d4992224258aeacd5979ab6eacd7becc2
kevin hu <hu.kevin.w@gmail.com>
2017-08-13 18:28:08 -0400

    Add function to show map for NewPost: Add indentation

--- src/components/map_job_post/MapJobPost.js
+++ src/components/map_job_post/MapJobPost.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import Map from './Map';
+// import Map from './Map';
+import Map from './GoogleMaps'
 import JobInfo from './JobInfo';
 import NewPost from './NewPost';
 
 class MapJobPost extends Component {
-  // switchcase for 3 components
-  notification(show)  {
-      switch(show) {
-          case 'map':
-              return <Map />;
-          case 'job_info':
-              return <JobInfo oneJob={this.props.mapJobPostData}/>;
-          case 'new_post':
-              return <NewPost mountApiGetData={this.props.mountApiGetData}/>;
-          default:
-              return null;
-      }
-  }
+    // switchcase for 3 components
+    notification(show) {
+        switch (show) {
+            case 'map':
+                return <Map/>;
+            case 'job_info':
+                return <JobInfo oneJob={this.props.mapJobPostData}/>;
+            case 'new_post':
+                return <NewPost mountApiGetData={this.props.mountApiGetData} showGoogleMap={this.props.showGoogleMap}/>;
+            default:
+                return null;
+        }
+    }
 
-  render() {
-    // return the desire component
-    return (
-      <div className='map_job_post_wrapper'>
-        <p>this is the map job and post compon </p>
-        {this.notification(this.props.renderType)}
-      </div>
-    )
-  };
+    render() {
+        // return the desire component
+        return (
+            <div className='map_job_post_wrapper'>
+                <p>this is the map job and post compon
+                </p>
+                {this.notification(this.props.renderType)}
+            </div>
+        )
+    };
 };
 
 export default MapJobPost
