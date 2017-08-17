25efcb7a195a53cf8c145af48aac78a618b402cd
kevin hu <hu.kevin.w@gmail.com>
2017-08-17 17:24:25 -0400

    Create put/patch function for editing jobPost

--- src/components/map_job_post/JobInfo.js
+++ src/components/map_job_post/JobInfo.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class JobInfo extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      editJob: false,
+      oneJob: {}
+    }
+  }
+  // need to mount the data for onejob to the state; only once
+  componentWillMount() {
+    this.state.oneJob = this.props.oneJob;
+  }
+
+  editJob() {
+    console.log("hello");
+    const jobID = this.props.oneJob.id;
+    const url = `http://localhost:3000/job_posts/${jobID}`
+    debugger
+    // axios.put('http://localhost:3000/job_posts/1', oneJob ).then( (res) => {
+    //   console.log("res ");
+    // }).catch( (err) => {
+    //   console.log(err);
+    // })
+  }
+
+
+
   render() {
-    const jobInfo = this.props.oneJob
-    return (
+    const { jobInfo, editJob }= this.state
+    let renderJob = null;
+    if (editJob === false ) {
+      renderJob =
         <div className="job_info_wrapper side_div">
           { Object.keys(jobInfo).length > 0 &&
             <div>
@@ -20,6 +49,27 @@ class JobInfo extends Component {
             </div>
           }
         </div>
+    } else {
+      renderJob =
+      <div className="job_info_wrapper side_div">
+        { Object.keys(jobInfo).length > 0 &&
+          <div>
+            <h3>{jobInfo.title}</h3>
+            <p>{jobInfo.company_name} - {jobInfo.location}</p>
+            <p>{jobInfo.summary}</p>
+            <p>Desire skills: {jobInfo.skills} </p>
+            <p>Salary: {jobInfo.lower_salary} - {jobInfo.upper_salary}</p>
+            <h4>Job Type: </h4>
+            <h4>Job Location: </h4>
+            <h4>Experience Requirement: </h4>
+            <p>Posted on: {jobInfo.date_created}</p>
+          </div>
+        }
+      </div>
+    }
+    debugger
+    return (
+      {renderJob}
     )
   };
 
