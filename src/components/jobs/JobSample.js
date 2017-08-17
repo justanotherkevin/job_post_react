936f9798811d984ab1981bfbf972d447f55f4ef7
kevin hu <hu.kevin.w@gmail.com>
2017-08-16 20:52:12 -0400

    Rearrange display filed for styling

--- src/components/jobs/JobSample.js
+++ src/components/jobs/JobSample.js
@@ -20,12 +20,14 @@ class JobSample extends Component {
         return (
             <div className="one_job">
                 <h3 onClick={this.mountJobInfo}>{job.title}</h3>
-                <p>{job.company_name}
-                    - {job.location}</p>
+                <p>{job.company_name}</p>
                 <p>{job.summary.split(' ').slice(0, 15).join(' ') + "..."}</p>
-                <p>Desire skills: {job.skills}
+                {/* <p>Desire skills: {job.skills}</p> */}
+                <p className="location_posted_date">
+                    <span>{job.location}</span>
+                    <span>job type</span>
+                    <span>{job.date_created}</span>
                 </p>
-                <p>Posted on: {job.date_created}</p>
             </div>
         )
     };
