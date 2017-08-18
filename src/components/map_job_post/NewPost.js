8e6c38b0556828865186689d5159ff79a53523e3
kevin hu <hu.kevin.w@gmail.com>
2017-08-17 21:22:01 -0400

    Create function for updating a job; Remove excessive function

--- src/components/map_job_post/NewPost.js
+++ src/components/map_job_post/NewPost.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-
+// This is also reused for edit/updating a post
 class NewPost extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            editingMode: false,
             newjob: {
                 title: "",
                 company_name: "",
@@ -19,9 +20,19 @@ class NewPost extends Component {
         };
         this.closeNewPost = this.closeNewPost.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.createJob = this.createJob.bind(this);
+        this.updateJob = this.updateJob.bind(this);
     }
 
+    componentWillMount() {
+        if (this.props.jobInfo) {
+          const jobInfo = this.props.jobInfo;
+          this.setState({newjob: jobInfo, editingMode: true});
+        } else {
+          console.log("not hello");
+        }
+    }
+    
     handleChange(event) {
         const targetClassName = event.target.className.split(" ")[0].replace("new_post_", "")
         const modNewjob = this.state.newjob
@@ -33,25 +44,37 @@ class NewPost extends Component {
         });
     }
 
-    handleSubmit(event) {
-        this.postJobToApi(this.state.newjob)
-        alert('A new post was submitted');
+    createJob(event) {
+        const newJob = this.state.newjob
+        const self = this;
+        axios.post('http://localhost:3000/job_posts', newJob).then( (response) => {
+            // after successful POST, mount the a GET call
+            self.props.mountApiGetData();
+            alert('A new post was submitted');
+        }).catch( (error) => {
+            alert('Seem like theres an error on our end');
+        });
+
         // post to rails api ==> make new job into jobpost database
         // recall database for new updated jobposts data
         // should then auto re-render with new wjob on top of all jobposts data state
         event.preventDefault();
     }
-    postJobToApi(data) {
 
+    updateJob(event) {
+        const thisJob = this.state.newjob
         const self = this;
-        axios.post('http://localhost:3000/job_posts', data).then(function(response) {
-            // after successful POST, mount the a GET call
+        const url = `http://localhost:3000/job_posts/${thisJob.id}`
+
+        axios.put(url, thisJob ).then( (res) => {
             self.props.mountApiGetData();
-        }).catch(function(error) {
-            // if there is an error?
+            alert('Job updated');
+        }).catch( (error) => {
             alert('Seem like theres an error on our end');
         });
+        event.preventDefault();
     }
+
     getDateTime() {
         const date = new Date()
         const datevalues = [
@@ -63,24 +86,25 @@ class NewPost extends Component {
         ];
         return datevalues;
     }
+
     closeNewPost() {
         this.props.showGoogleMap();
     }
 
     render() {
-
+        const submitForm = this.state.editingMode === true ? this.updateJob : this.createJob
         return (
             <div className="new_post_wrapper">
                 <button onClick={this.closeNewPost} >close me </button>
                 <h3 className="new_post_header">This is posting a job!</h3>
 
-                <form className="new_post_form" onSubmit={this.handleSubmit}>
+                <form className="new_post_form" onSubmit={submitForm}>
 
                     <div className="input_group">
                         <input
                             className={"new_post_title form_control " + (this.state.newjob['title'] === ''? '' : 'has_value') }
                             type="text"
-                            value={this.state.title}
+                            value={this.state.newjob.title}
                             onChange={this.handleChange}/>
                         <label>Post Title:</label>
                     </div>
@@ -88,7 +112,7 @@ class NewPost extends Component {
                     <div className="input_group">
                         <input
                             className={'new_post_company_name form_control ' + (this.state.newjob['company_name'] === ''? '' : 'has_value') }
-                            value={this.state.company_name}
+                            value={this.state.newjob.company_name}
                             onChange={this.handleChange}/>
                         <label>Company Name:</label>
                     </div>
@@ -96,7 +120,7 @@ class NewPost extends Component {
                     <div className="input_group">
                         <input
                             className={"new_post_location form_control " + (this.state.newjob['location'] === ''? '' : 'has_value') }
-                            value={this.state.location}
+                            value={this.state.newjob.location}
                             onChange={this.handleChange}/>
                         <label>Location:</label>
                     </div>
@@ -104,7 +128,7 @@ class NewPost extends Component {
                     <div className="input_group">
                         <input
                             className={"new_post_skills form_control " +  (this.state.newjob['skills'] === ''? '' : 'has_value') }
-                            value={this.state.skills}
+                            value={this.state.newjob.skills}
                             onChange={this.handleChange}/>
                         <label>Desire Skills:</label>
                     </div>
@@ -112,7 +136,7 @@ class NewPost extends Component {
                     <div className="input_group">
                         <textarea
                             className={"new_post_summay form_control " + (this.state.newjob['summary'] === ''? '' : 'has_value') }
-                            value={this.state.summary}
+                            value={this.state.newjob.summary}
                             onChange={this.handleChange}/>
                         <label>Summary:</label>
                     </div>
