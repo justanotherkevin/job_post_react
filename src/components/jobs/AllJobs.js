99f4dd5e2e738bd14c02968d094f94f14b1251bb
kevin hu <hu.kevin.w@gmail.com>
2017-08-18 21:54:18 -0400

    Create function to set focus job

--- src/components/jobs/AllJobs.js
+++ src/components/jobs/AllJobs.js
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import JobSample from './JobSample';
 
 class AllJobs extends Component {
+    constructor() {
+        super();
+        this.state = {
+            focusJob: ""
+        };
+        this.setFocusJob = this.setFocusJob.bind(this);
+    };
 
-  render() {
-    // better for performance if it was not set in variable first
-    // const eachJob = this.props.allJobs.map(( job, index ) =>
-    //   <JobSample key={ job.id } job={ job } changeStateJobInfoData={ this.props.changeStateJobInfoData }/>
-    // )
+    setFocusJob(job_id) {
+        this.setState( {focusJob: job_id} );
+    };
 
-    return (
-      <div className="all_jobs_wrapper">
-        <div className="scroll_wrapper">
+    render() {
+        // better for performance if it was not set in variable first
+        // const eachJob = this.props.allJobs.map(( job, index ) =>
+        //   <JobSample key={ job.id } job={ job } changeStateJobInfoData={ this.props.changeStateJobInfoData }/>
+        // )
+        const focus_job = this.state.focusJob
+        return (
+            <div className="all_jobs_wrapper">
+                <div className="scroll_wrapper">
 
-          {this.props.allJobs.map(
-            ( job, index ) =>
-            <JobSample key={ job.id } job={ job } showOneJob={ this.props.showOneJob }/>
-          )}
-        </div>
-      </div>
-    )
-  };
+                    {this.props.allJobs.map((job, index) =>
+                        <JobSample
+                            key={job.id}
+                            job={job}
+                            showOneJob={this.props.showOneJob}
+                            setFocusJob={this.setFocusJob}
+                            focusJob={focus_job}
+                        />
+                    )}
+                </div>
+            </div>
+        )
+    };
 };
 
 export default AllJobs
