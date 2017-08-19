4114cea81ea57a60f0cfdf2ef23eef8669536cda
kevin hu <hu.kevin.w@gmail.com>
2017-08-18 23:26:33 -0400

    Add last clicked job class to render

--- src/components/jobs/JobSample.js
+++ src/components/jobs/JobSample.js
@@ -4,25 +4,23 @@ class JobSample extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            job: []
-        };
         this.mountJobInfo = this.mountJobInfo.bind(this)
     };
-    componentWillMount() {}
+
     mountJobInfo() {
         const {job} = this.props
         this.props.showOneJob(job);
+        this.props.setFocusJob(job.id)
     };
 
     render() {
-        const {job} = this.props
+        const { job, focusJobId } = this.props
+        const focusJobClass = focusJobId === job.id ? "active_job" : "nope"
         return (
-            <div className="one_job">
+            <div className={"one_job "+ focusJobClass}>
                 <h3 onClick={this.mountJobInfo}>{job.title}</h3>
                 <p>{job.company_name}</p>
                 <p>{job.summary.split(' ').slice(0, 15).join(' ') + "..."}</p>
-                {/* <p>Desire skills: {job.skills}</p> */}
                 <p className="location_posted_date">
                     <span>{job.location}</span>
                     <span>job type</span>
