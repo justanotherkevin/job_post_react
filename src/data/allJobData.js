08fb023e19c27498655b3af48e84863e6a757fac
kevin hu <hu.kevin.w@gmail.com>
2018-01-17 09:09:07 -0500

    Fix hard coded data

--- src/data/allJobData.js
+++ src/data/allJobData.js
@@ -2,101 +2,26 @@ const allJobs = [
   {
   id: 1,
   title: "First Post",
+  company_name: "company_name",
+  location: "location",
   body: "An Airplane",
+  summary: "Bacon ipsum dolor amet leberkas sirloin kielbasa, shankle spare ribs sausage ribeye shank t-bone pastrami. Picanha chuck capicola boudin, prosciutto turducken kevin alcatra drumstick leberkas pork chop frankfurter porchetta. Tri-tip fatback ball tip pork chop tongue capicola salami buffalo. Pastrami filet mignon kevin, swine ground round hamburger tail capicola ball tip spare ribs. Pork belly shankle swine, turducken salami capicola sirloin cupim chicken corned beef brisket filet mignon short loin burgdoggen landjaeger.",
+  date_created: "date created",
   created_at: "2017-08-09T21:29:47.633Z",
   updated_at: "2017-08-09T21:29:47.633Z"
   },
   {
-  id: 2,
-  title: "Second Post",
-  body: "A Train",
-  created_at: "2017-08-09T21:29:47.639Z",
-  updated_at: "2017-08-09T21:29:47.639Z"
-  },
-  {
-  id: 3,
-  title: "Third Post",
-  body: "A Truck",
-  created_at: "2017-08-09T21:29:47.643Z",
-  updated_at: "2017-08-09T21:29:47.643Z"
-  },
-  {
-  id: 4,
-  title: "Fourth Post",
-  body: "A Boat",
-  created_at: "2017-08-09T21:29:47.649Z",
-  updated_at: "2017-08-09T21:29:47.649Z"
-  },
-  {
-  id: 5,
-  title: "Investor Community-Services Analyst",
-  body: "Reiciendis natus omnis explicabo. Rem omnis officiis. Alias aut illum veniam corrupti libero accusantium. Ipsa sit reiciendis cum accusamus harum facere nemo.",
-  created_at: "2017-08-09T21:29:48.368Z",
-  updated_at: "2017-08-09T21:29:48.368Z"
-  },
-  {
-  id: 6,
-  title: "Retail Liaison",
-  body: "Recusandae laudantium eos et quo distinctio pariatur vitae. Laborum qui non qui. Dolores harum sed autem rem neque eum. Debitis expedita unde consequuntur.",
-  created_at: "2017-08-09T21:29:48.372Z",
-  updated_at: "2017-08-09T21:29:48.372Z"
-  },
-  {
-  id: 7,
-  title: "Product Technician",
-  body: "Omnis fugiat aliquam optio reprehenderit molestiae eum. Qui et nesciunt modi quisquam tenetur.",
-  created_at: "2017-08-09T21:29:48.377Z",
-  updated_at: "2017-08-09T21:29:48.377Z"
-  },
-  {
-  id: 8,
-  title: "Legacy Marketing Technician",
-  body: "Aliquam non omnis velit. Et deserunt id eos sit deleniti. Eum corporis qui deserunt dicta.",
-  created_at: "2017-08-09T21:29:48.381Z",
-  updated_at: "2017-08-09T21:29:48.381Z"
-  },
-  {
-  id: 9,
-  title: "Regional Representative",
-  body: "Architecto voluptatem qui ipsum at. Cumque dolorem officia. Et quibusdam rerum natus. Quia molestias odit iure est ea.",
-  created_at: "2017-08-09T21:29:48.386Z",
-  updated_at: "2017-08-09T21:29:48.386Z"
-  },
-  {
-  id: 10,
-  title: "District Retail Coordinator",
-  body: "Fugit beatae alias et voluptatem quia hic. Provident assumenda esse. Sit occaecati fugit at fugiat. Reiciendis velit aliquam sunt et.",
-  created_at: "2017-08-09T21:29:48.389Z",
-  updated_at: "2017-08-09T21:29:48.389Z"
-  },
-  {
-  id: 11,
-  title: "Regional Strategist",
-  body: "Magnam cum rerum laboriosam dolores. Non velit debitis harum quis ipsa. Sit non nobis perferendis. Reiciendis est iure eos ducimus beatae non eius.",
-  created_at: "2017-08-09T21:29:48.392Z",
-  updated_at: "2017-08-09T21:29:48.392Z"
-  },
-  {
-  id: 12,
-  title: "District Orchestrator",
-  body: "Veniam ut adipisci vel magnam voluptatem fugiat quis. Perferendis consequuntur aut ratione similique commodi. Placeat aut rerum ea deserunt. Dolores exercitationem et necessitatibus quisquam autem voluptatum cumque.",
-  created_at: "2017-08-09T21:29:48.396Z",
-  updated_at: "2017-08-09T21:29:48.396Z"
-  },
-  {
-  id: 13,
-  title: "Chief Government Producer",
-  body: "Aliquid et dolores necessitatibus. Consectetur aut commodi. Sequi delectus fugiat laboriosam nemo velit illo quos. Consequuntur omnis eaque quo est voluptas nihil.",
-  created_at: "2017-08-09T21:29:48.400Z",
-  updated_at: "2017-08-09T21:29:48.400Z"
-  },
-  {
-  id: 14,
-  title: "Regional Assistant",
-  body: "Sunt error architecto et qui cupiditate. Cum nesciunt vel quidem. Et perferendis error aut quo architecto. Delectus inventore praesentium distinctio.",
-  created_at: "2017-08-09T21:29:48.407Z",
-  updated_at: "2017-08-09T21:29:48.407Z"
+    id: 2,
+    title: "Secound Post",
+    company_name: "company_name",
+    location: "location",
+    body: "An Airplane",
+    summary: "Bacon ipsum dolor amet leberkas sirloin kielbasa, shankle spare ribs sausage ribeye shank t-bone pastrami. Picanha chuck capicola boudin, prosciutto turducken kevin alcatra drumstick leberkas pork chop frankfurter porchetta. Tri-tip fatback ball tip pork chop tongue capicola salami buffalo. Pastrami filet mignon kevin, swine ground round hamburger tail capicola ball tip spare ribs. Pork belly shankle swine, turducken salami capicola sirloin cupim chicken corned beef brisket filet mignon short loin burgdoggen landjaeger.",
+    date_created: "date created",
+    created_at: "2017-08-09T21:29:47.633Z",
+    updated_at: "2017-08-09T21:29:47.633Z"
   }
+  
 ];
 
 export default allJobs;
